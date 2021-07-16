39f9593b19a16340cf976083bff6314ee2ba220c
khjeong <jkha7371@gmail.com>
2021-07-16 16:31:24 +0900

    Mnist DNN Test

--- SLXORPerctron.py
+++ SLXORPerctron.py
@@ -8,15 +8,18 @@ if device == 'cuda':
 X = torch.FloatTensor([[0,0],[0,1],[1,0],[1,1]]).to(device)
 Y = torch.FloatTensor([[0],[1],[1],[0]]).to(device)
 
-linear = nn.Linear(2,1,bias=True)
-sigmoid = nn.Sigmoid()
-model = nn.Sequential(linear,sigmoid).to(device)
+model = nn.Sequential(
+    nn.Linear(2,2,bias=True)
+    ,nn.Sigmoid()
+    ,nn.Linear(2,1,bias=True)
+    ,nn.Sigmoid()
+).to(device)
 
 # loss function & optimizer
 criterion = torch.nn.BCELoss().to(device)
 optimizer = torch.optim.SGD(model.parameters(),lr=1)
 
-nb_epochs = 1000
+nb_epochs = 2000
 
 for epoch in range(nb_epochs):
     optimizer.zero_grad()
